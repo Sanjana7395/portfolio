427e6ecf1497cdc000dd4f174e12ff327d0736a3
Sanjana7395 <sanjanasrinivas73@gmail.com>
2020-12-23 13:52:19 +0530

    Updates

---
+++ precache-manifest.2d480ac3aea6458791ae20e731024167.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fd8e3788bed2deb794030523f3c4e1",
+    "url": "/Sanjana7395/portfolio/index.html"
+  },
+  {
+    "revision": "3067298e0615891e2884",
+    "url": "/Sanjana7395/portfolio/static/css/main.0c120a85.chunk.css"
+  },
+  {
+    "revision": "b2ee7be6edb76273a11d",
+    "url": "/Sanjana7395/portfolio/static/js/2.73443402.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/Sanjana7395/portfolio/static/js/2.73443402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3067298e0615891e2884",
+    "url": "/Sanjana7395/portfolio/static/js/main.97149918.chunk.js"
+  },
+  {
+    "revision": "d550de44bfbfb7ea05c4",
+    "url": "/Sanjana7395/portfolio/static/js/runtime-main.d36045e0.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/Sanjana7395/portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/Sanjana7395/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/Sanjana7395/portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/Sanjana7395/portfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/Sanjana7395/portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/Sanjana7395/portfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/Sanjana7395/portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/Sanjana7395/portfolio/static/media/react.f370c1e0.png"
+  }
+]);
